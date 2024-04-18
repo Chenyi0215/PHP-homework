ea8bb4b5cfaa0f6cb290695a1af515920ccadd8b
Chenyi0215 <153057530+Chenyi0215@users.noreply.github.com>
2024-04-18 12:14:01 +0800

    Add files via upload

---
+++ php2024midterm/author.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+<meta charset="utf-8">
+</head>
+
+<form action="authorshow.php" method="post">
+
+<center><h1>Author您好,歡迎進入論文投稿網頁</h1></center>
+論文標題:</br>
+<input type="text" name="sTitle" value="" placeholder="Please enter your title"></br>
+作者姓名:<input type="text" name="sName" value="" placeholder="Please enter your name"></br>
+作者Email:<input type="email" name="sEmail" placeholder="Please enter your email"><br/>
+論文摘要：
+<textarea name="sMytxt" style="width: 50%; height: 10em ">
+</textarea>
+</br>
+
+<input type="submit" value="提交">
+</form>
+</html>
---
+++ php2024midterm/showreview.php
@@ -0,0 +1,9 @@
+<meta charset=utf8>
+
+<?php
+
+$sGender=$_post["sGender"];
+echo $sGender."<br/>";
+$sMytxt=$_post["sMytxt"];
+echo $sMytxt."<br/>";
+?>
